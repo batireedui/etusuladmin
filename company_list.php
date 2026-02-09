4af77270699dc0de77a1999a0aeae62411c3ee55
Javkhlan BatIreedui <j.batireedui@gmail.com>
2026-02-09 08:10:39 +0800

    ss

--- company_list.php
+++ company_list.php
@@ -1,6 +1,7 @@
  <?php
     require_once 'header.php';
     require_once 'sidebar.php';
+    $statusArr = ['paid' => 'Идэвхтэй', 'registered' => 'Бүртгүүлсэн', 'expired' => 'Хугацаа дууссан'];
     ?>
  <div class="page-wrapper">
      <!-- ============================================================== -->
@@ -19,11 +20,13 @@
                                  <th>РД</th>
                                  <th>Утасны дугаар</th>
                                  <th>Бүртгэсэн огноо</th>
+                                 <th>Төлөв</th>
+                                 <th>Үйлдэл</th>
                              </tr>
                          </thead>
                          <tbody>
                              <?php
-                                $query = "SELECT comID, comName, RD, phone, ognoo FROM company ORDER BY comID DESC";
+                                $query = "SELECT comID, comName, RD, phone, ognoo, status, domainName FROM company ORDER BY comID DESC";
                                 _selectNoParam(
                                     $stmt,
                                     $count,
@@ -32,17 +35,40 @@
                                     $comName,
                                     $RD,
                                     $phone,
-                                    $ognoo
+                                    $ognoo,
+                                    $status,
+                                    $domainName
                                 );
                                 $count = 1;
-                                while (_fetch($stmt)) {
-                                    echo "<tr>";
-                                    echo "<td>" . $count++ . "</td>";
-                                    echo "<td>" . $comName . "</td>";
-                                    echo "<td>" . $RD . "</td>";
-                                    echo "<td>" . $phone . "</td>";
-                                    echo "<td>" . $ognoo . "</td>";
-                                    echo "</tr>";
+                                while (_fetch($stmt)) { ?>
+                                 <tr>
+                                     <td>
+                                         <?php echo $count++; ?>
+                                     </td>
+                                     <td>
+                                         <?php echo $comName; ?>
+                                         <br>
+                                         <span class="font-12 text-nowrap d-block ">
+                                             <?php 
+                                             if ($domainName == null) {
+                                                 $domainName = 'Домэйн нэр байхгүй';
+                                             }
+                                             else {
+                                                 $domainName = '<a href="' . $domainName . '" target="_blank">' . $domainName . '</a>';
+                                             }
+                                             echo $domainName; ?>
+                                         </span>
+                                     </td>
+                                     <td><?php echo $RD; ?></td>
+                                     <td><?php echo $phone; ?></td>
+                                     <td><?php echo $ognoo; ?></td>
+                                     <td>
+                                         <span class="badge bg-<?php echo $status == 'paid' ? 'success' : ($status == 'registered' ? 'warning' : 'danger'); ?> font-12 text-white font-weight-medium badge-pill border-0"><?php echo $statusArr[$status]; ?></span>
+                                     <td class="text-center">
+                                         <button class="btn btn-sm waves-effect waves-light btn-rounded btn-primary border-0">Тохиргоо</button>
+                                     </td>
+                                 </tr>
+                             <?php
                                 }
                                 ?>
                          </tbody>
